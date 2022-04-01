0e5bb9a3899b8474bfcb01a7bc52d34ebb8e6dce
Mourad Rahi <dodirahi@gmail.com>
2022-04-02 00:23:11 +0300

    in this commit i started designing the second page of the app and i implemented the spinner i also added a nice little easter egg that consists of clicking on the image view and it plays the sound of a coin bag

--- AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
+++ AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
@@ -3,12 +3,18 @@ package com.lau.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class Convert extends AppCompatActivity {
+public class Convert extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convert);
+
     }
+
 }
--- AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -3,23 +3,34 @@ package com.lau.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private ImageView image;
+    MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=(Button) findViewById(R.id.next);
+        image = (ImageView) findViewById(R.id.imageView2);
     }
 
     public void start(View v){
         Intent intent= new Intent(getApplicationContext(),Convert.class);
         startActivity(intent);
+    }
+    public void play (View v){
+        if (player == null){
+            player = MediaPlayer.create(getApplicationContext(),R.raw.coindrop);
+        }
+        player.start();
     }
 
 }
