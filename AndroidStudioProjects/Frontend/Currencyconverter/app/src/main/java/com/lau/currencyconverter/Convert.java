04ae1d4704ec45ffb57f60da3650131bf3c0083e
Mourad Rahi <dodirahi@gmail.com>
2022-04-02 00:53:23 +0300

    i started working on my convert method but the electricity went out and i had to stop so i am commiting this in case and writing this as a reminder to continue from where i stoped

--- AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
+++ AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
@@ -7,14 +7,32 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Convert extends AppCompatActivity  {
-
+public class Convert extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convert);
+        spinner = (Spinner)  findViewById(R.id.value);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(getApplicationContext(),R.array.value, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
     }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+    public void convert(View v){
+        String s=spinner.getSelectedItem()+"";
+        
+    }
 }
