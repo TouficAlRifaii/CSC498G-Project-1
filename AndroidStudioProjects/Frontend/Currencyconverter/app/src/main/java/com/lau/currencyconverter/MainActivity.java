d7a0d9f987645ad635af357a757ca4f8ab2cc1c0
Mourad Rahi <dodirahi@gmail.com>
2022-04-01 23:12:33 +0300

    in this commit i created the onclick function of the first button that takes us to the second activity and of course i also made the second activity

--- AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -2,14 +2,24 @@ package com.lau.currencyconverter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button) findViewById(R.id.next);
+    }
+
+    public void start(View v){
+        Intent intent= new Intent(getApplicationContext(),Convert.class);
+        startActivity(intent);
     }
 
 }
