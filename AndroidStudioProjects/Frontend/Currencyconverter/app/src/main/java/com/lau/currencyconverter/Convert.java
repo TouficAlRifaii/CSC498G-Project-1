ee3839b73c7fb1864d625773afb2693f535389e8
Mourad Rahi <dodirahi@gmail.com>
2022-04-02 13:27:14 +0300

    in this commit i wrote the convert method without the api just to have a blueprint and be able to complete the project faster once the api is complete

--- AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
+++ AndroidStudioProjects/Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
@@ -6,20 +6,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.InputMismatchException;
 
 public class Convert extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Spinner spinner;
+    private EditText enter;
+    private TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_convert);
         spinner = (Spinner)  findViewById(R.id.value);
-        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(getApplicationContext(),R.array.value, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(getApplicationContext(),
+                R.array.value, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+        result=(TextView) findViewById(R.id.result);
+        enter=(EditText) findViewById(R.id.input);
 
     }
     @Override
@@ -33,6 +42,27 @@ public class Convert extends AppCompatActivity implements AdapterView.OnItemSele
     }
     public void convert(View v){
         String s=spinner.getSelectedItem()+"";
-        
+        try{
+            double input=Double.parseDouble(enter.getText().toString());
+            if(enter.getText().length()!=0){
+                if (s.equals("USD")){
+                    result.setText(input*22000 +"LBP");
+                    result.setAlpha(1.0F);
+                }
+                else if (s.equals("LBP")){
+                    result.setText(input/22000 +"USD");
+                    result.setAlpha(1.0F);
+                }
+                else{
+                    Toast.makeText(this, "Enter a currency", Toast.LENGTH_SHORT).show();
+                }
+            }
+            else{
+                Toast.makeText(this, "Enter a number", Toast.LENGTH_SHORT).show();
+            }
+        }catch (NumberFormatException e){
+            Toast.makeText(this, "Enter a number", Toast.LENGTH_SHORT).show();
+        }
     }
+
 }
