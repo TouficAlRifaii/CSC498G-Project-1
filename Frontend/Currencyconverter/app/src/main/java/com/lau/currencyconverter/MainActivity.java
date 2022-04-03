08a76647662a1f27fca44e0919a2721af9f8c5cc
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-04-03 22:37:19 +0300

    creating the download Task class

--- Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/MainActivity.java
+++ Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/MainActivity.java
@@ -4,12 +4,68 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        protected String doInBackground(String... urls){
+            String rate = "";
+            URL url;
+            HttpURLConnection http;
+
+            try{
+                url = new URL(urls[0]);
+                http = (HttpURLConnection) url.openConnection();
+
+                InputStream in = http.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while( data != -1){
+                    char current = (char) data;
+                    rate += current;
+                    data = reader.read();
+
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+            Log.i("rate" , rate);
+            return rate;
+        }
+
+
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            try{
+                JSONObject json = new JSONObject(s);
+                String rate = json.getString("rate");
+                Log.i("rate", rate);
+
+            }catch(Exception e){
+                Log.i("WTF" , "WTF");
+            }
+        }
+
+    }
+
+
     //i am creating my variables that will be used in the future methods
     private Button button;
     private ImageView image;
@@ -18,6 +74,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String url = "http://192.168.11.108/CSC498G-Project-1/backend/api.php";
+        DownloadTask task = new DownloadTask();
+        task.execute(url);
         button=(Button) findViewById(R.id.next);
         image = (ImageView) findViewById(R.id.imageView2);
     }
