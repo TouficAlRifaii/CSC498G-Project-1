ae2356fd0ca0ef6f0d5a4db569eff98430b2d593
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-04-03 18:50:52 +0300

    fixing the 1 appearing after the json object

--- backend/api.php
+++ backend/api.php
@@ -2,7 +2,8 @@
 $url = "https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t20224317";
 
 $curl = curl_init($url);
-
+curl_setopt($curl , CURLOPT_URL , $url);
+curl_setopt($curl , CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($curl);
 curl_close($curl);
 echo $response;
