13e9d066ad93ae1208a7a40295325b2b92ebbac3
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-04-04 00:04:32 +0300

    performing the calculations to convert

--- backend/post.php
+++ backend/post.php
@@ -10,6 +10,16 @@ $currency = $_POST["currency"];
 $query = $mysqli -> ("INSERT INTO conversions (amount, rate, currency) Values(?, ?, ?)");
 $query -> bind_param("dds", $amount , $rate , $currency);
 
+$array_response = [];
 
+if (strcmp($currency, "lbp") === 0) { 
+    $array_response["result"] = $amount / $rate ; 
+    $query->execute();
+} else if (strcmp($currency , "usd") === 0 ){ 
+    $array_response["result"] = $amount * $rate ; 
+    $query->exceute(); 
+}
+
+echo json_encode($array_response);
 
 ?>
