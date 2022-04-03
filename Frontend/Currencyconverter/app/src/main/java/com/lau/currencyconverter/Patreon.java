0ff43853df436c9873e37d8571f6d255b532ee5a
Mourad Rahi <dodirahi@gmail.com>
2022-04-03 21:30:59 +0300

    in this commit i just added an animation to the thank you page

--- Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Patreon.java
+++ Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Patreon.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Patreon extends AppCompatActivity {
 
@@ -12,9 +14,13 @@ public class Patreon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patreon);
+        ImageView wendy= (ImageView) findViewById(R.id.wendy);
+
+        wendy.animate().alpha(1.0F).setDuration(2000);
     }
     public void change(View v){//takes us back to the conversion page
         Intent intent= new Intent(getApplicationContext(),Convert.class);
         startActivity(intent);
     }
+
 }
