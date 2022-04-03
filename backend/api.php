653a4972ff7c93bb71c1009ab656a6e26c3b60f2
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-04-03 18:45:13 +0300

    Curling the api

--- backend/api.php
+++ backend/api.php
@@ -1,5 +1,10 @@
 <?php 
+$url = "https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t20224317";
 
-echo ("Hello world");
+$curl = curl_init($url);
+
+$response = curl_exec($curl);
+curl_close($curl);
+echo $response;
 
 ?>
