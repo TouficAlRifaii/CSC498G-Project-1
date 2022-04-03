96f18cd09137e239691ab2a7ec8a10aaf6114bd8
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-04-03 20:29:32 +0300

    Extracting the rate

--- backend/api.php
+++ backend/api.php
@@ -1,11 +1,17 @@
 <?php 
-$url = "https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t20224317";
+$url = "https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t20224320";
 
 $curl = curl_init($url);
 curl_setopt($curl , CURLOPT_URL , $url);
 curl_setopt($curl , CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($curl);
+
 curl_close($curl);
-echo $response;
+$stdClass = (json_decode($response));
+$arr = json_decode(json_encode($stdClass) , true) ;
+//echo $response;
+echo gettype($arr);
+$rate = $arr['omt'][count($arr['omt']) -1][1]; 
+echo $rate;
 
 ?>
