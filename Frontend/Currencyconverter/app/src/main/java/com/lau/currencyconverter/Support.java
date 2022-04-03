bff9a365eb77ae0447fc814aad9500831bfec49b
Mourad Rahi <dodirahi@gmail.com>
2022-04-03 21:09:53 +0300

    i added our team track when clicking on our team logo

--- Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Support.java
+++ Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Support.java
@@ -3,15 +3,23 @@ package com.lau.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Support extends AppCompatActivity {
 
+    MediaPlayer player;
+    private boolean isPlaying;
+    private ImageView logo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_support);
+        isPlaying=false;
+        logo=(ImageView) findViewById(R.id.logo);
     }
     public void change(View v){//this takes us back to the conversion page
         Intent intent= new Intent(getApplicationContext(),Convert.class);
@@ -20,5 +28,23 @@ public class Support extends AppCompatActivity {
     public void patreon(View v){//this takes us to the placeholder patreon page
         Intent intent= new Intent(getApplicationContext(),Patreon.class);
         startActivity(intent);
+    }
+    public void ft(View v) {//this method is used when we click on the small image in the lower corner to activate the easter egg
+        if (player == null) {
+            player = MediaPlayer.create(getApplicationContext(), R.raw.fairytail);
+        }
+        if (isPlaying){
+            player.stop();
+            isPlaying = false;
+            player=null;
+            
+        }
+        else {
+
+            player.start();
+            isPlaying=true;
+
+        }
+
     }
 }
