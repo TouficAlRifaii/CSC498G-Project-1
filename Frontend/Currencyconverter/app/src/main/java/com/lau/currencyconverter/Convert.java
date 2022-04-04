fccfa50a70bec18baca5b4f45313d05f98e11d7f
Toufic Al Rifaii <toufic.alrifaii@lau.edu>
2022-04-04 03:14:12 +0300

    done with displaying the result

--- Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
+++ Frontend/Currencyconverter/app/src/main/java/com/lau/currencyconverter/Convert.java
@@ -210,8 +210,7 @@ public class Convert extends AppCompatActivity implements AdapterView.OnItemSele
     public void onNothingSelected(AdapterView<?> adapterView) {
         button.setEnabled(false);
     }
-
-    //this method is to be modified after the completion of the api since we need to send it there compute it and get it back
+    
     public void convert(View v){
         Log.i("rate" , rate);
         String s=spinner.getSelectedItem()+"";
@@ -219,17 +218,14 @@ public class Convert extends AppCompatActivity implements AdapterView.OnItemSele
             double input=Double.parseDouble(enter.getText().toString());
             if(enter.getText().length()!=0){
                 if (s.equals("USD")){
-                   // String concatenated=input * Integer.parseInt(rate)  +"LBP";
+
                     sendPostRequest(Double.toString(input) , rate ,s.toLowerCase(Locale.ROOT));
-//                    result.setText(concatenated);
-//                    result.animate().alpha(1.0F);
 
                 }
                 else if (s.equals("LBP")){
-                  //  String concatenated=input/Integer.parseInt(rate) +"USD";
+
                     sendPostRequest(Double.toString(input) , rate ,s.toLowerCase(Locale.ROOT));
-//                    result.setText(concatenated);
-//                    result.animate().alpha(1.0F);
+
                 }
                 else{
                     Toast.makeText(this, "Enter a currency", Toast.LENGTH_SHORT).show();
@@ -260,20 +256,15 @@ public class Convert extends AppCompatActivity implements AdapterView.OnItemSele
 
                 HttpClient httpClient = new DefaultHttpClient();
 
-                // In a POST request, we don't pass the values in the URL.
-                //Therefore we use only the web page URL as the parameter of the HttpPost argument
+
                 HttpPost httpPost = new HttpPost("http://192.168.11.108/CSC498G-Project-1/backend/post.php");
 
-                // Because we are not passing values over the URL, we should have a mechanism to pass the values that can be
-                //uniquely separate by the other end.
-                //To achieve that we use BasicNameValuePair
-                //Things we need to pass with the POST request
-                BasicNameValuePair amountBasicNameValuePair = new BasicNameValuePair("paramAmount", paramAmount);
-                BasicNameValuePair rateBasicNameValuePAir = new BasicNameValuePair("paramRate", paramRate);
-                BasicNameValuePair currencyBasicNameValuePAir = new BasicNameValuePair("paramCurrency", paramCurrency);
 
-                // We add the content that we want to pass with the POST request to as name-value pairs
-                //Now we put those sending details to an ArrayList with type safe of NameValuePair
+                BasicNameValuePair amountBasicNameValuePair = new BasicNameValuePair("amount", paramAmount);
+                BasicNameValuePair rateBasicNameValuePAir = new BasicNameValuePair("rate", paramRate);
+                BasicNameValuePair currencyBasicNameValuePAir = new BasicNameValuePair("currency", paramCurrency);
+
+
                 List<NameValuePair> nameValuePairList = new ArrayList<NameValuePair>();
                 nameValuePairList.add(amountBasicNameValuePair);
                 nameValuePairList.add(rateBasicNameValuePAir);
@@ -281,20 +272,16 @@ public class Convert extends AppCompatActivity implements AdapterView.OnItemSele
 
 
                 try {
-                    // UrlEncodedFormEntity is an entity composed of a list of url-encoded pairs.
-                    //This is typically useful while sending an HTTP POST request.
+
                     UrlEncodedFormEntity urlEncodedFormEntity = new UrlEncodedFormEntity(nameValuePairList);
 
-                    // setEntity() hands the entity (here it is urlEncodedFormEntity) to the request.
-                    httpPost.setEntity(urlEncodedFormEntity);
+                   httpPost.setEntity(urlEncodedFormEntity);
 
                     try {
-                        // HttpResponse is an interface just like HttpPost.
-                        //Therefore we can't initialize them
+
                         HttpResponse httpResponse = httpClient.execute(httpPost);
 
-                        // According to the JAVA API, InputStream constructor do nothing.
-                        //So we can't initialize InputStream although it is not an interface
+
                         InputStream inputStream = httpResponse.getEntity().getContent();
 
                         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -312,7 +299,7 @@ public class Convert extends AppCompatActivity implements AdapterView.OnItemSele
                         return stringBuilder.toString();
 
                     } catch (ClientProtocolException cpe) {
-                        System.out.println("First Exception case of HttpResponese :" + cpe);
+                        System.out.println("First Exception case of HttpResponse :" + cpe);
                         cpe.printStackTrace();
                     } catch (IOException ioe) {
                         System.out.println("Second Exception case of HttpResponse :" + ioe);
@@ -332,9 +319,11 @@ public class Convert extends AppCompatActivity implements AdapterView.OnItemSele
                 super.onPostExecute(s);
 
                 try{
+                    Log.i("s" , s);
                     JSONObject json = new JSONObject(s);
                     strResult = json.getString("result");
                     result.setText(strResult);
+                    result.animate().alpha(1.0F);
                     Log.i("result" , strResult);
 
 
